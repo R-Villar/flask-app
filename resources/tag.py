5ceb81113e9bafac6a8d037b69a9ef4643265e92
Remberto Villar <rembertovillar@gmail.com>
2023-03-06 22:08:10 -0500

    added user register endpoint

--- resources/tag.py
+++ resources/tag.py
@@ -8,7 +8,7 @@ from schemas import TagSchema, TagAndItemSchema
 
 blp = Blueprint("Tags", "tags", description="Operations on tags")
 
-@blp.route("/store/<string:store_id>/tag")
+@blp.route("/store/<int:store_id>/tag")
 class TagsInStore(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self, store_id):
@@ -31,7 +31,7 @@ class TagsInStore(MethodView):
 
 
 
-@blp.route("/item/<string:items_id>/tag/<string:tag_id>")
+@blp.route("/item/<int:items_id>/tag/<int:tag_id>")
 class LinkTagsToItem(MethodView):
     @blp.response(201, TagSchema)
     def post(self, item_id, tag_id):
@@ -65,7 +65,7 @@ class LinkTagsToItem(MethodView):
         return {"message": "Item removed from tag", "item": item, "tag": tag}
 
 
-@blp.route("/tag/<string:tag_id>")
+@blp.route("/tag/<int:tag_id>")
 class Tag(MethodView):
     @blp.response(200, TagSchema)
     def get(self, tag_id):
